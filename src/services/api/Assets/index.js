fd2131f1a725b647d84ca1680bd62b4cd43ba9c8
Vinay Rawat <vineyrawat@yahoo.com>
2021-07-15 20:11:13 +0530

    Project completed

--- src/services/api/Assets/index.js
+++ src/services/api/Assets/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const instance = axios.create({ baseURL: "http://localhost:5500" });
+const instance = axios.create({ baseURL: "https://crypto-tracker-vinay.herokuapp.com/" });
 
 class AssetRequest {
   getAll = async () => {
--- src/services/api/Auth/index.js
+++ src/services/api/Auth/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5500/auth",
+  baseURL: "https://crypto-tracker-vinay.herokuapp.com/auth",
 });
 
 const login = async (email, password) => {
